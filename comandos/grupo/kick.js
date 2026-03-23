ba40b328c44aa6db88cdbdc78203af5c26323b1b
Jose C <josecacerescaceres69@gmail.com>
2026-03-23 01:20:25 -0600

    Actualizar kick.js

--- comandos/grupo/kick.js
+++ comandos/grupo/kick.js
@@ -10,23 +10,25 @@ export default {
     try {
       const m = mentionUser(msg, sender)
 
-      const target = msg.message?.extendedTextMessage?.contextInfo?.participant
+      const target =
+        msg.message?.extendedTextMessage?.contextInfo?.participant ||
+        msg.message?.extendedTextMessage?.contextInfo?.mentionedJid?.[0] ||
+        msg.message?.contextInfo?.participant
 
       if (!target) {
         return sock.sendMessage(from, {
-          text: '➭ Responde al mensaje del usuario que deseas expulsar'
+          text: '➭ Responde o menciona al usuario que deseas expulsar'
         })
       }
 
       const targetUser = getUser(target)
 
-      
       const targetIsAdmin = isAdmin(target, participants)
       const senderIsOwner = isOwner(sender)
 
       if (targetIsAdmin && !senderIsOwner) {
         return sock.sendMessage(from, {
-          text: `➭ No puedes expulsar a un administrador`
+          text: '➭ No puedes expulsar a un administrador'
         })
       }
 
