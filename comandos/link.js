8caa8871e2f61dad2af28441ddc2a04cee8cdfe1
Jose C <josecacerescaceres69@gmail.com>
2026-03-22 18:26:33 -0600

    Actualizar link.js

--- comandos/link.js
+++ comandos/link.js
@@ -4,27 +4,11 @@ export default {
   admin: true,
   botAdmin: true,
 
-  run: async ({ sock, from, sender, isAdmin, isOwner, botAdmin }) => {
+  run: async ({ sock, from, sender }) => {
     console.log('📥 COMANDO LINK EJECUTADO')
-    console.log({
-      from,
-      sender,
-      isAdmin,
-      isOwner,
-      botAdmin
-    })
 
     try {
-      if (!botAdmin) {
-        console.log('❌ BOT NO ES ADMIN')
-        return await sock.sendMessage(from, {
-          text: '❌ El bot debe ser administrador para obtener el link'
-        })
-      }
-
       const code = await sock.groupInviteCode(from)
-      console.log('✅ CODE:', code)
-
       const link = `https://chat.whatsapp.com/${code}`
 
       await sock.sendMessage(from, {
