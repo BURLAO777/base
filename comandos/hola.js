54bee93d3f62478b10fc23a1f63fe609776eecaf
Jose C <josecacerescaceres69@gmail.com>
2026-03-23 19:59:24 -0600

    Actualizar hola.js

--- comandos/hola.js
+++ comandos/hola.js
@@ -2,7 +2,7 @@ export default {
   name: 'hola',
   run: async ({ sock, from }) => {
     await sock.sendMessage(from, {
-      text: '👋 Hola desde comandos'
+      text: '👋 Hola a todos desde comandos'
     })
   }
 }
